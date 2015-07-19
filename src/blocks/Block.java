58980633e250cda72f4ea0b2eff7423144adef3c
AdrianJakobsen <adrjak.aj@gmail.com>
2015-07-19 16:19:02 +0200

    Testing laptop to stat

--- src/blocks/Block.java
+++ src/blocks/Block.java
@@ -1,7 +1,12 @@
 package blocks;
 
+import javafx.scene.paint.Color;
+
 /**
  * Created by Adrian on 17.07.2015.
  */
 public class Block {
+
+    private Color colorOfBlock;
+    private String BlockType;
 }
