7f3ec53e93c29e42f6ea7201bc9f71de12b74531
Adrian <adrjak.aj@gmail.com>
2015-07-19 16:01:38 +0200

    Testing this out

--- src/HighScore.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Adrian on 18.07.2015.
- */
-public class HighScore {
-}
---
+++ src/User.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+/**
+ * Created by Adrian on 18.07.2015.
+ */
+public class User {
+
+    private String name;
+    private Date date;
+    private int score;
+
+    public User() {
+
+    }
+}
