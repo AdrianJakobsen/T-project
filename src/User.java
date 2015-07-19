dbf0760846c942fbe781adbcba59c01d33516004
Adrian <adrjak.aj@gmail.com>
2015-07-19 16:04:04 +0200

    Testing this out

--- src/User.java
+++ src/User.java
@@ -9,7 +9,37 @@ public class User {
     private Date date;
     private int score;
 
-    public User() {
 
+
+    public User(String name, Date date, int score) {
+        this.name =name;
+        this.date =date;
+        this.score = score;
+
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
